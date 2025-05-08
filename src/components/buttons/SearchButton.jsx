1b3caa635655d9ed3c072db1b578a62df653eaf0
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-08 18:47:28 +0300

    remove the whitespace, and use the props that has been pass from the parent component

--- src/components/buttons/SearchButton.jsx
+++ src/components/buttons/SearchButton.jsx
@@ -1,8 +1,8 @@
 
-export default function SearchButton() {
+export default function SearchButton({searchSongs}) {
   return (
     <div className="flex justify-center">
-      <button className="bg-[#28d7a3] hover:bg-[#1da57c] transition-all duration-100 text-black font-bold py-3 px-4 rounded-full cursor-pointer min-w-32 max-w-[60%] w-full ">SEARCH</button>
+      <button className="bg-[#28d7a3] hover:bg-[#1da57c] transition-all duration-100 text-black font-bold py-3 px-4 rounded-full cursor-pointer min-w-32 max-w-[60%] w-full " onClick={searchSongs}>SEARCH</button>
     </div>
   );
 }
