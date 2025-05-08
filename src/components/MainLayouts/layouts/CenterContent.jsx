d679d8e5f98db37823e2989e892be310b2ef2dd2
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-08 18:45:02 +0300

    import the function that fetches data, create a function for event listeners, pass the functions as  props to the components

--- src/components/MainLayouts/layouts/CenterContent.jsx
+++ src/components/MainLayouts/layouts/CenterContent.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 import SearchBar from "../../inputs/SearchBar";
 import ListContent from "./features/ListContent";
 import StoredContent from "./features/StoredContent";
 
+import getToken from "./features/fetchData/getToken";
+import searchTrack from "./features/fetchData/searchTrack";
 export default function CenterContent() {
+  const [playlists, setPlaylists] = useState();
+  const [song, setSong] = useState();
+
+  const searchSongs = async () => {
+    const accessToken = await getToken();
+    const data = await searchTrack(accessToken, song);
+    setPlaylists(data);
+  };
+
+  const handleSongs = (e) => {
+    e.preventDefault();
+    setSong(e.target.value);
+  };
   return (
     <div className="text-white bg-slate-700 pb-10">
-      <SearchBar />
+      <SearchBar searchSongs={searchSongs} handleSongs={handleSongs}/>
       <div className="flex justify-center gap-10 pt-14 flex-wrap px-3">
-        <ListContent />
+        <ListContent playlists={playlists} handleSongs={handleSongs} setArtist={setArtist} setSong={setSong}/>
         <StoredContent />
       </div>
     </div>
