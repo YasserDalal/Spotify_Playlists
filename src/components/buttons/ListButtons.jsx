0967c7abc8e924cbe3aa773f548742b831228f56
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-09 18:16:37 +0300

    add lazy load and improve the loading performance of images when fetching

--- src/components/buttons/ListButtons.jsx
+++ src/components/buttons/ListButtons.jsx
@@ -4,7 +4,7 @@ export default function ListButton({handleSongs, playlist}) {
     <li className="p-2 rounded-lg">
       <div className="flex rounded-lg cursor-pointer p-2 hover:shadow-[0px_1.8px_1px_2px] hover:shadow-slate-700 duration-100 " onClick={handleSongs}>
         <div className="w-36">
-          <img src={playlist.album.images[1].url} alt="singers picture" className="rounded-full"/>
+          <img src={playlist.album.images[1]?.url || playlist.album.images[0]?.url} loading="lazy" alt="singers picture" className="rounded-full"/>
         </div>
         <div className="flex flex-col justify-center pl-4 w-full">
           <div className="font-bold text-xl w-full text-[clamp(0.8rem,3.6vw,1.3rem)]">{playlist.name}</div>
