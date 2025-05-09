5a2fe4d19e3ac40df1ffe4b262653ac9022ebd93
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-09 18:08:38 +0300

    create a mimic skeleton of the lists when loading

--- src/components/loading UI/CardSkeleton.jsx
+++ src/components/loading UI/CardSkeleton.jsx
@@ -2,6 +2,14 @@ import { Skeleton } from "../ui/skeleton"
 
 export default function LoadingCards(){
   return (
-    <Skeleton className="h-96 w-full" />
+    <div className="flex items-center gap-4 py-5 pl-5">
+      <div className="w-20 h-auto">
+        <Skeleton className=" h-20 rounded-full" />
+      </div>
+      <div>
+        <Skeleton className="h-5 w-44 mb-2"/>
+        <Skeleton className="h-5 w-20" />
+      </div>
+    </div>
   )
 }
