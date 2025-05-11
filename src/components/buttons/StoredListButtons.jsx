52fe5a590036875056d0ccd4d0f8439a0b5e6003
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-11 17:36:11 +0300

    add responsive font sizes to both ListButtons and StoredButtons

--- src/components/buttons/StoredListButtons.jsx
+++ src/components/buttons/StoredListButtons.jsx
@@ -8,8 +8,8 @@ export default function StoredListButton({playlist, handleRemove}) {
           <img src={playlist.album.images[1]?.url || playlist.album.images[0]?.url} alt="singers picture" className="rounded-full"/>
         </div>
         <div className="flex flex-col justify-center pl-4  w-full">
-          <div className="font-bold text-xl">{playlist.name}</div>
-          <div className="text-gray-400">{playlist.artists[0].name}</div>
+          <div className="font-bold text-xl text-[clamp(0.9rem,4vw,1.3rem)]">{playlist.name}</div>
+          <div className="text-gray-400 text-[clamp(0.7rem,4vw,1rem)]">{playlist.artists[0].name}</div>
         </div>
         <div className="flex justify-end items-center">
           <div className="text-5xl">-</div>
