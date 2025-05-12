1e1fd3c59ca0552fa6b7f0ae131f4da0d56aae7c
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-12 20:26:45 +0300

    remove the useEffect hook, import the localStorage functions and use it in forms, add comments for future reference

--- src/components/MainLayouts/MainLayout.jsx
+++ src/components/MainLayouts/MainLayout.jsx
@@ -1,25 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 import Form from '../login/Form.jsx'
 import MainContent from './MainContent.jsx'
 
+import saveStorage from '../localStorage/saveStorage.js';
+import getStorage from '../localStorage/getStorage.js';
 export default function MainLayout(){
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-  useEffect(() => {
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-    if (isLoggedIn === 'true') {
-      setIsLoggedIn(true);
-    }
-  }, []);
+  // at first render this state will be 'null' so it will execute the else statement and set it to 'false'
+  const [isLoggedIn, setIsLoggedIn] = useState(getStorage('isLoggedIn') || false);
 
   const handleLogin = () => {
-    setIsLoggedIn(true)
-    localStorage.setItem('isLoggedIn', true)
+    // the isLoggedIn state will be set to 'true'
+    saveStorage('isLoggedIn', true);
   }
+
   return (
     <div className='w-full h-full bg-slate-900'>
-      {isLoggedIn ? <MainContent />  : <Form handleLogin={handleLogin} />}
+      {isLoggedIn ? <MainContent /> : <Form handleLogin={handleLogin} />}
     </div>
-  )
+  );
+
 }
