b7d76ebcaff94e7107f9f7148301eb3792191e7b
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-17 18:47:27 +0300

    reduced the z index of BottomContent component from 50 to 40

--- src/components/MainLayouts/layouts/BottomContent.jsx
+++ src/components/MainLayouts/layouts/BottomContent.jsx
@@ -4,7 +4,7 @@ import Watermark from "./features/DetailsContent";
 export default function BottomContent({ className }) {
   return (
     <div className={className}>
-      <Watermark className="pt-5 text-[14px] text-gray-400 opacity-60 select-none pointer-events-none z-50 text-center"/>
+      <Watermark className="pt-5 text-[14px] text-gray-400 opacity-60 select-none pointer-events-none z-40 text-center"/>
     </div>
   );
 }
