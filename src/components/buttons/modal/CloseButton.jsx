c1650ea4a27cfaf53a35a613596e523fc29b783e
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-18 21:15:47 +0300

    change the text from 'Close' to '×' of CloseButton child component of ModalButtonsContainer component

--- src/components/buttons/modal/CloseButton.jsx
+++ src/components/buttons/modal/CloseButton.jsx
@@ -2,7 +2,7 @@
 export default function CloseButton({ className, handleModal }) {
   return (
     <div className={className} onClick={handleModal}>
-      <button>Close</button>
+      <button className="text-6xl text-[#e37575] hover:text-[#ff4e4e]">×</button>
     </div>
   );
 }
