c8d91dbba2b586e2eb3daa2263129a698532e3cd
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-05-26 16:20:39 +0300

    for development purposes

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,18 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Spotify_Playlists/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 5178, // Baguhin ito sa port na nais mo
+    allowedHosts: [
+      'revised-girlfriend-same-designed.trycloudflare.com', // Add your Ngrok URL here
+      'localhost', // If you want to keep localhost as an allowed host
+    ],
   },
 })
